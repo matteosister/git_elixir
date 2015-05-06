f1deff7d50e9a27ae1f3af3b264c6ac54a526b58
Matteo Giachino <matteog@gmail.com>
2015-05-06 22:38:42 +0200

    unstage and commit

--- lib/git_elixir/command.ex
+++ lib/git_elixir/command.ex
@@ -15,8 +15,10 @@ defmodule GitElixir.Command do
   Example
     iex> GitElixir.Command.init
     ["init"]
+
     iex> GitElixir.Command.init(true)
     ["--bare", "init"]
+
     iex> GitElixir.Command.init(100)
     ["init"]
   """
@@ -31,9 +33,59 @@ defmodule GitElixir.Command do
   Example
     iex> GitElixir.Command.add
     ["add", "."]
+
     iex> GitElixir.Command.add("src/")
     ["add", "src/"]
   """
   def add(what \\ "."), do: ~w{ add #{what} }
+
+  @doc """
+  Remove a node from the stage and put in the working tree
+
+  Example
+    iex> GitElixir.Command.unstage
+    ["reset", "HEAD"]
+
+    iex> GitElixir.Command.unstage("src/")
+    ["reset", "HEAD", "src/"]
+  """
+  def unstage(what \\ nil)
+  def unstage(what) when is_nil(what), do: ~w{ reset HEAD }
+  def unstage(what), do: ~w{ reset HEAD #{what} }
+
+
+  @doc """
+  Commit the current staged files
+
+  Example
+    iex> GitElixir.Command.commit("")
+    ** (ArgumentError) empty commit message
+
+    iex> GitElixir.Command.commit(" ")
+    ** (ArgumentError) empty commit message
+
+    iex> GitElixir.Command.commit("test message")
+    ["commit", "-m", "test message"]
+
+    iex> GitElixir.Command.commit("test message", true)
+    ["commit", "-a", "-m", "test message"]
+
+    iex> GitElixir.Command.commit("test message", true, "matteo")
+    ["commit", "-a", "--author", "matteo", "-m", "test message"]
+
+    iex> GitElixir.Command.commit("test message", true, "matteo", true)
+    ["commit", "-a", "--author", "matteo", "--allow-empty", "-m", "test message"]
+  """
+  def commit(msg, all \\ false, author \\ nil, allow_empty \\ false)
+  def commit(msg, all, author, allow_empty) do
+    if (String.strip(msg) == "") do
+      raise ArgumentError, message: "empty commit message"
+    end
+    args = ~w{commit}
+    if all, do: args = args ++ ~w{-a}
+    unless is_nil(author), do: args = args ++ ["--author", author]
+    if allow_empty, do: args = args ++ ~w{--allow-empty}
+    args = args ++ ["-m", msg]
+  end
 
 end
--- lib/git_elixir/command/caller.ex
+++ lib/git_elixir/command/caller.ex
@@ -3,9 +3,22 @@ defmodule GitElixir.Command.Caller do
   Given a command, execute it and returns a list of output lines
   """
   def call(%GitElixir.Repo{path: path}, args) do
-    System.cmd("git", args, [cd: path])
-    |> split_lines
-    |> Enum.filter(fn (line) -> line != "" end)
+    pid = spawn_link(__MODULE__, :do_call, [])
+    send pid, { self, [args, [cd: path]] }
+    receive do
+      { :ok, lines } ->
+        lines
+    end
+  end
+
+  defp do_call do
+    receive do
+      { sender, [args, [cd: path]] } ->
+        lines = System.cmd("git", args, [cd: path])
+        |> split_lines
+        |> Enum.filter(fn (line) -> line != "" end)
+        send sender, { :ok, lines }
+    end
   end
   
   defp split_lines({output, 0}), do: String.split(output, ~r{\n}, trim: true)
