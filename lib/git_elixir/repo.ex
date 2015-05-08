8e271650b5f5889d4b2636c874c15f0fb07be1bb
Matteo Giachino <matteog@gmail.com>
2015-05-08 23:05:05 +0200

    alias and stage

--- lib/git_elixir/repo.ex
+++ lib/git_elixir/repo.ex
@@ -2,21 +2,29 @@ defmodule GitElixir.Repo do
   
   alias GitElixir.Command.Caller
   alias GitElixir.Command
+  alias GitElixir.Repo
 
   defstruct path: "", name: ""
 
   @doc """
   Given a repository struct creates the containing folder
   """
-  def create_dir(%GitElixir.Repo{path: path}) do
+  def create_dir(%Repo{path: path}) do
     System.cmd "mkdir", [path]
   end
 
   @doc """
   Given a repository struct calls the init command
   """
-  def init(repo = %GitElixir.Repo{}) do
+  def init(repo = %Repo{}) do
     Caller.call repo, Command.init
+  end
+
+  @doc """
+  Given a repository and a path, adds the given path to the staging area
+  """
+  def stage(repo = %Repo{}, path \\ ".") do
+    Caller.call repo, Command.add(path)
   end
   
 end
